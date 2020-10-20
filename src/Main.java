8d167692d7de803f4e3f3fa52b2b1af5f28b762a
BitXplorer <dan.danielsson@gmail.com>
2020-10-20 13:55:52 +0200

    Added several methods to access and store player data. Still have some to go.

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,22 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Main {
-
-    public static void addNewPlayers(ArrayList<Player> loadedPlayers){
-        String name;
-        int wins = 0;
-        int totalPlayed = 0;
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Write your name: ");
-        name = in.next();
-        loadedPlayers.add(new Player(name,wins,totalPlayed));
-    }
 
     public static void main(String[] args) throws IOException,ClassNotFoundException {
 
         Player.start();
-        //currentPlayers = Player.loadPlayersFromFile();
 
-        /*
-        Player p1 = new Player("Test",5,10);
+
+        Player p1 = new Player("Woop",5,10);
         Player p2 = new Player("test2",30,40);
         Player p3 = new Player("Harry",3,5);
 
-        currentPlayers.add(p1);
-
-         */
-        //Player now = currentPlayers.get(0);
-
-
-        //System.out.print(now.toString());
-
-
-        //addNewPlayers(currentPlayers);
-
+        //Player.addPlayer(p2);
+        Player.showAllPlayers();
         //Player.savePlayersToFile();
+
+        Player.savePlayersToFile();
 
 
 
--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,11 @@
 import java.io.*;
-import java.util.AbstractList;
 import java.util.ArrayList;
 
 
 public class Player implements Serializable {
 
-    private static AbstractList<Player> currentPlayers;
-    private String name;
+        private static ArrayList<Player> currentPlayers = new ArrayList<>();
+        private String name;
         private int wins;
         private int totalPlayed;
 
@@ -22,31 +21,28 @@ public class Player implements Serializable {
         }
 
         public static void start() throws IOException, ClassNotFoundException {
-             Player.currentPlayers = loadPlayersFromFile();
-        }
-/*
-        public ArrayList<Player> getPlayer(ArrayList<Player> currentPlayers){
-
-        for(int i=0;i<currentPlayers.size()-1;i++){
+            currentPlayers = loadPlayersFromFile();
 
         }
 
+        public static void addPlayer(Player newPlayer){
+            currentPlayers.add(newPlayer);
         }
 
- */
+        public static void showAllPlayers(){
+            System.out.println(currentPlayers);
+        }
 
 
 
-
-         public static void savePlayersToFile(ArrayList<Player> currentPlayers) throws IOException {
+         public static void savePlayersToFile() throws IOException {
             ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("storedPlayers.txt"));
             output.writeObject(currentPlayers);
-            System.out.print(currentPlayers);
             output.close();
 
         }
 
-        public ArrayList<Player> loadPlayersFromFile() throws IOException, ClassNotFoundException {
+        public static ArrayList<Player> loadPlayersFromFile() throws IOException, ClassNotFoundException {
             ObjectInputStream input = new ObjectInputStream(new FileInputStream("storedPlayers.txt"));
             ArrayList<Player> loadedPlayers = new ArrayList<>();
 
